0b86c54a02cee8f7129f7ddfa3acb0bc9327917d
DELL <ducag0409@gmail.com>
2021-05-29 16:11:21 +0700

    temperature convert

---
+++ app/src/main/java/com/example/tempconverter/ConverterClass.java
@@ -0,0 +1,17 @@
+package com.example.tempconverter;
+
+public class ConverterClass {
+    public double c2f_fn(double celsius){
+
+        return (celsius*1.8)+32;
+
+    }
+
+
+    public double f2c_fn(double farenheit){
+
+        return (farenheit-32)/1.8;
+
+
+    }
+}
